539f167d8918088482d521ad4c038dc48f0a646b
AMAN SINGH <2amansingh07@gmail.com>
2025-09-01 22:59:33 +0530

    updating the dashboard

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -14,18 +14,9 @@ export default function Dashboard() {
   return (
     <>
     <div>devloper is revamping the dashboard soon it will get updated but still you can use it...</div>
-      <div className="w-full min-h-screen bg-gradient-to-br from-pink-100 to-slate-300">
-        <div className="  w-full h-16 border-b-2 border-black">
-          <div className="flex items-center justify-between  py-2 px-10 ">
-            <div className="flex">
-            <div className="w-6 h-6 bg-blue-600 rounded-full"></div>
-            <span className="text-xl font-bold text-gray-800">Quickdraw</span>
-             
-            </div>
-            <DropdownMenuDemo/>
+      
           
-          </div>
-        </div>
+        
 
         <main>
 
@@ -44,7 +35,7 @@ export default function Dashboard() {
             <Canvascards />
           </div>
         </main>
-      </div>
+      
     </>
   );
 }
